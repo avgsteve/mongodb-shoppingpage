7eab895f30804660b24b6a32c05a275fcfb73f37
avgsteve <steve.leng@gmail.com>
2020-07-23 18:21:37 +0800

    add user sign up functionality ( using bcrypt.compare() )
    Ref for using bcrypt.compare as Promise in .then/.catch blocks:  https://www.npmjs.com/package/bcrypt#with-promises
    
    ref on Udemy:
    https://www.udemy.com/course/mongodb-the-complete-developers-guide/learn/lecture/11861344#questions

--- shell-to-driver-01-starting-code/backend/routes/auth.js
+++ shell-to-driver-01-starting-code/backend/routes/auth.js
@@ -15,17 +15,61 @@ const createToken = () => {
 };
 
 router.post('/login', (req, res, next) => {
+
+  // 1) get user's email and password (not hashed)
   const email = req.body.email;
   const pw = req.body.password;
-  // Check if user login is valid
-  // If yes, create token and return it to client
-  const token = createToken();
-  // res.status(200).json({ token: token, user: { email: 'dummy@dummy.com' } });
-  res
-    .status(401)
-    .json({
-      message: 'Authentication failed, invalid username or password.'
-    });
+
+  console.log(`\nLog-in attempt: ${email}\n`);
+
+  // 2) use email to find user's document
+  dbConnection.getDbConnection().db().collection('users').findOne({
+    email: email
+  }).then(userDoc => {
+
+      console.log(`\nUser with email: ${email} found in database! Verifying user's password now ...\n\n`);
+
+      // 3) Check if user login is valid by using bcrypt.compare(plainPassword, hashedPassword) to validate plain password from user with the hashed password from database
+      return bcrypt.compare(pw, userDoc.password);
+      // bcrypt.compare() will return true if validation is successful
+
+      // ref for using bcrypt.compare as Promise in .then/.catch blocks:  https://www.npmjs.com/package/bcrypt#with-promises
+    }
+
+  ).then(verificationResult => {
+      // 4) varify the login process with the result of bcrypt.compare
+
+      if (!verificationResult) { // when result is false, throw an error
+        console.log(`\nPassword not match!\n`);
+
+        throw Error();
+      }
+      // if not false (which is true), send a token to user
+
+      console.log(`\nUser: ${email} log in successful! \n`);
+
+      const token = createToken();
+      res.status(200).json({
+        message: 'Authentication succeeded.',
+        token: token
+      });
+    }
+
+    // 5) if any error happens, catch it and send 401 HTTP response and fail message
+  ).catch(err => //
+    {
+      console.log(`\nUser's log-in attempt has failed!! The error message is:\n`);
+      console.log(err);
+
+      res
+        .status(401)
+        .json({
+          message: 'Authentication failed, invalid username or password.'
+        });
+    }
+  );
+
+  //end of router.post('/login', (req, res, next) => {
 });
 
 router.post('/signup', (req, res, next) => {
